be53abb756db536413d831d6ebce8e8983f806a2
Igor <example@gmail.com>
2022-06-24 23:07:46 +0300

    add task one

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('hello');
+//task №1
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 10);
+
+
+//task №2
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+};
+console.log(personalMovieDB);
+
+
+//task №3
+const lastFilm = prompt('Один из последних просмотренных фильмов?', 'Суховей'),
+      yourRating = prompt('На сколько оцените его?', '8'),
+         
+      lastFilmtwo = prompt('Один из последних просмотренных фильмов?', 'Драйв'),
+      yourRatingtwo = prompt('На сколько оцените его?', '6');
+
+   
+personalMovieDB.movies[lastFilm] = yourRating;
+
+personalMovieDB.movies[lastFilmtwo] = yourRatingtwo;
+
+console.log(personalMovieDB);
